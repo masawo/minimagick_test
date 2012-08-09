34d91b29e16bb21da3d09324abed5e19b4aca9f0
masawo <masawo@gmail.com>
2012-08-09 17:36:24 +0900

    alternative

--- images/convert.sh
+++ images/convert.sh
@@ -1,2 +1,3 @@
-# http://www.randomsequence.com/articles/overlaying-images-with-imagemagick/
 convert food.jpeg overlay.png -gravity northeast -composite -format jpg -quality 75 output.jpg
+#以下でも同じ
+#composite -gravity northeast overlay.png food.jpeg -format jpg -quality 75 output2.jpg
